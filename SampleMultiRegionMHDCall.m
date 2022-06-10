6fc206b75496b08535abbde214d83e45dbba38e3
paul-reiners <paul.reiners@gmail.com>
2022-06-10 17:52:36 -0500

    Simplified code.

--- SampleMultiRegionMHDCall.m
+++ SampleMultiRegionMHDCall.m
@@ -1,21 +1,16 @@
+MAXI=128;       
 n = 10;
-roi_1 = zeros(n, 3);
-roi_1(:,1)= randi(128, n, 1);
-roi_1(:,2)= randi(128, n, 1);
-roi_1(:, 3) = randi(128, n, 1);
-
+c=3;
+roi_1=randi(MAXI,n,c);
 n = 8;
-roi_2 = zeros(n, 3);
-roi_2(:,1)= randi(128, n, 1);
-roi_2(:,2)= randi(128, n, 1);
-roi_2(:, 3) = randi(128, n, 1);
-
-% Note that the "spread-outness" of a single region is 0.0 as you would
-% expect.
-dist = MultiRegionMHD({roi_1; roi_2});
-fprintf('Result of first example: %f\n', dist)
+roi_2=randi(MAXI,n,c);
 
 % Note that regions can have different number of 'voxels' but each point
 % must have the same dimension (3, in this case).
+dist = MultiRegionMHD({roi_1; roi_2});
+fprintf('Result of first example: %f\n', dist)
+
+% Note that the "spread-outness" of a single region is 0.0 as you would
+% expect.
 dist = MultiRegionMHD([roi_1]);
 fprintf('Result of second example: %f\n', dist)
